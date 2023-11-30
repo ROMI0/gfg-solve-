ccc3e916f42298353d5efa3c78002800af386f1c
ROMIO <111486321+ROMI0@users.noreply.github.com>
2023-11-30 18:19:16 +0530

    Create Shortest path from 1 to n.java

---
+++ Shortest path from 1 to n.java
@@ -0,0 +1,16 @@
+class Solution{
+    public int minStep(int n) {
+        int steps = 0;
+        
+        while (n > 1) {
+            if (n % 3 == 0) {
+                n /= 3;
+            } else {
+                n--;
+            }
+            steps++;
+        }
+        
+        return steps;
+    }
+}
